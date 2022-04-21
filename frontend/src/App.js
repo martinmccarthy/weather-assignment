7f0958b684d4d0abafa8efdba26fdc95d7588c1e
martin <martinmccarthy@knights.ucf.edu>
2022-04-21 01:14:44 -0400

    adds working api search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
+import axios from 'axios';
 
 function App() {
   const [days, setDays] = useState(1);
   const [city, setCity] = useState("");
   const [cards, setCards] = useState([]);
+  const [info, setInfo] = useState([]);
+  
   const cityHandler = (event) => {
     setCity(event.target.value);
   };
@@ -13,26 +16,34 @@ function App() {
     setDays(event.target.value);
   };
 
-  function populate() {
+  async function getCards() {
     var temp = [];
-    for(var i = 0; i < days; i++) {
-      var cardInfo = {
-        min_temp: 0,
-        max_temp: 50,
-        avg_temp: 41,
-        date: "2022-04-21",
-        condition: "sunny",
-        city: "london"
+    console.log(process.env.REACT_APP_KEY);
+    await axios({
+      method: 'get',
+      url: 'http://api.weatherapi.com/v1/forecast.json',
+      params: {key: process.env.REACT_APP_KEY, q: city, days: days}
+    }).then(function(response) {
+      var res = response.data;
+      var forecast = res.forecast.forecastday;
+      for(var i = 0; i < forecast.length; i++) {
+        var dayInfo = {
+          city: res.location.name,
+          date: forecast[i].date,
+          min_temp: forecast[i].day.mintemp_f,
+          max_temp: forecast[i].day.maxtemp_f,
+          avg_temp: forecast[i].day.avgtemp_f,
+          condition: forecast[i].day.condition.text
+        }
+        temp.push(dayInfo);
       }
-      temp.push(cardInfo);
-    };
+    })
     setCards(temp);
   }
 
   function doSearch() {
     // pull the items and then populate the cards
-    populate();
-    
+    getCards();
 
   };
 
@@ -54,8 +65,6 @@ function App() {
         </select>
         <button type="button" onClick={doSearch}>Submit</button>
       </form>
-      <h1>{days}</h1>
-      <h1>{city}</h1>
       <ul>
         {cards.map((card) => (
           <li>
