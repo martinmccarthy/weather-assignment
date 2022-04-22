604ca07567be0f6d15b212b3d6ffe7918495e541
martin <martinmccarthy@knights.ucf.edu>
2022-04-22 19:34:51 -0400

    adds heroku path

--- frontend/src/Path.js
+++ frontend/src/Path.js
@@ -3,6 +3,6 @@ const app_name = '' // change to heroku
 exports.buildPath = function buildPath(route) {
     // if we're on the host environment then api calls go there,
     // otherwise api calls are to the localhost
-    if(process.env.NODE_ENV == 'production') return 'https://' + app_name + '.herokuapp.com/' + route;
+    if(process.env.NODE_ENV == 'production') return 'https://weatherproject01.herokuapp.com/' + route;
     else return 'http://localhost:5000' + route;
 }
